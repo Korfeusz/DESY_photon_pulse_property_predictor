f2ff9f506d9f4ef2d343c78d0c46d520b54cf31c
Korfeusz <brockhuis.leo@gmail.com>
2019-07-22 17:21:29 +0200

    add possibility to open beam profile dsets

--- file_tools.py
+++ file_tools.py
@@ -1,13 +1,17 @@
 from os import listdir
 from os.path import isfile, join
 import path_constants
+import h5py
 
 
 def get_paths_to_files_in_directory(directory):
     return [join(directory, f) for f in listdir(directory) if isfile(join(directory, f))]
 
 
-def get_run_path(run_no=0):
+def get_run_path(run_number=0):
     directory = path_constants.experiment_data_directory
-    return join(directory, get_paths_to_files_in_directory(directory)[run_no])
+    return join(directory, get_paths_to_files_in_directory(directory)[run_number])
 
+
+def get_beam_profiles(current_run):
+    return current_run[path_constants.beam_profiles_path]
