15cbf9a6fbb512fe8f9fa42a695b9f4e763322ce
Korfeusz <brockhuis.leo@gmail.com>
2019-07-22 17:10:31 +0200

    add file tools for .h5 file access and add path constants file

---
+++ file_tools.py
@@ -0,0 +1,13 @@
+from os import listdir
+from os.path import isfile, join
+import path_constants
+
+
+def get_paths_to_files_in_directory(directory):
+    return [join(directory, f) for f in listdir(directory) if isfile(join(directory, f))]
+
+
+def get_run_path(run_no=0):
+    directory = path_constants.experiment_data_directory
+    return join(directory, get_paths_to_files_in_directory(directory)[run_no])
+
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import file_tools
 import h5py
 
 if __name__ == '__main__':
-    print('hello, is this working?')
+    current_run = file_tools.get_run_path(0)
+    print(current_run)
+    run_file = h5py.File(current_run, 'r')
+
+    run_file.close()
