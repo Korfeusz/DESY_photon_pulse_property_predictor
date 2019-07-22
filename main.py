ebbe0f12e423642dd45df9d31578a4243bf2d44f
Leo Brockhuis <brockhul@max-p3a004.desy.de>
2019-07-22 17:21:29 +0200

    add possibility to open beam profile dsets

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import file_tools
 import h5py
 
-if __name__ == '__main__':
-    current_run = file_tools.get_run_path(0)
-    print(current_run)
-    run_file = h5py.File(current_run, 'r')
 
-    run_file.close()
+if __name__ == '__main__':
+    current_run_path = file_tools.get_run_path(0)
+    with h5py.File(current_run_path, 'r') as current_run:
+        beam_profiles = file_tools.get_beam_profiles(current_run)
+        print(beam_profiles.size)
