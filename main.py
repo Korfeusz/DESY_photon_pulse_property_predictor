30198191b18ed2d18b6445816b7f0231e6aeb89c
Leo Brockhuis <brockhul@max-p3a004.desy.de>
2019-07-23 09:35:22 +0200

    change colormap to jet

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import file_tools
 import h5py
 import matplotlib.pyplot as plt
+from matplotlib import cm
 
 if __name__ == '__main__':
-    current_run_path = file_tools.get_run_path(1)
+    current_run_path = file_tools.get_run_path(run_number=8)
     with h5py.File(current_run_path, 'r') as current_run:
-        beam_profiles = file_tools.get_beam_profiles(current_run)
-        plt.imsave('save_test.png', beam_profiles[499, :, :])
+        beam_profiles = file_tools.get_beam_profiles(run=current_run)
+        plt.imsave('save_test.png', beam_profiles[400, 94:350, 94:350], cmap=cm.jet)
+
