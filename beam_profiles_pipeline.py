9cfa8e6e5bdedf394fea09f68da91bd7452e8a85
Leo Brockhuis <brockhul@max-p3a008.desy.de>
2019-07-24 11:58:04 +0200

    changed beam_profile_creator to a pipeline for all beam profiles

--- beam_profiles_pipeline.py
+++ beam_profiles_pipeline.py
@@ -2,22 +2,23 @@ import constants
 import numpy as np
 
 
-class BeamProfileCreator:
+class BeamProfilesPipeline:
     def __init__(self, data, color_resolution=constants.BEAM_PROFILE_COLOR_RESOLUTION):
         self.beam_profile_data = data
         self.shape = self.beam_profile_data.shape
         self.color_resolution = color_resolution
 
     def slice_horizontally(self, h_min, h_max):
-        return BeamProfileCreator(data=self.beam_profile_data[:, h_min:h_max])
+        return BeamProfilesPipeline(data=self.beam_profile_data[:, :, h_min:h_max])
 
     def slice_vertically(self, v_min, v_max):
-        return BeamProfileCreator(data=self.beam_profile_data[v_min:v_max, :])
+        return BeamProfilesPipeline(data=self.beam_profile_data[:, v_min:v_max, :])
 
     def change_color_resolution(self, new_resolution):
         data = self.beam_profile_data / self.color_resolution * new_resolution
         color_resolution = new_resolution
-        return BeamProfileCreator(data, color_resolution)
+        return BeamProfilesPipeline(data, color_resolution)
 
-    def get_rounded_beam_profile(self):
+    def get_rounded_beam_profiles(self):
         return np.round(self.beam_profile_data)
+
--- main.py
+++ main.py
@@ -8,11 +8,11 @@ if __name__ == '__main__':
     horizontal_min, horizontal_max = 0, 230
     vertical_min, vertical_max = 0, 200
 
-    beam_profile = file_tools.get_beam_profile_creator(run_number=run_number, profile_number=image_number)\
+    beam_profile = file_tools.get_beam_profiles_pipeline(run_number=run_number)\
         .slice_horizontally(h_min=horizontal_min, h_max=horizontal_max)\
         .slice_vertically(v_min=vertical_min, v_max=vertical_max)\
         .change_color_resolution(desired_color_bits)\
-        .get_rounded_beam_profile()
+        .get_rounded_beam_profiles()
 
-    beam_profile_imaging.save_beam_profile_image(beam_profile, name='save_test.png')
-    print(beam_profile.max())
+    beam_profile_imaging.save_beam_profile_image(beam_profile[10, :, :], name='save_test.png')
+    print(beam_profile.shape)
