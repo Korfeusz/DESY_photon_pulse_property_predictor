524782b840cfe21ce0b56f29b00c0913ae988c52
Korfeusz <brockhuis.leo@gmail.com>
2019-07-24 11:58:04 +0200

    changed beam_profile_creator to a pipeline for all beam profiles

--- file_tools.py
+++ file_tools.py
@@ -1,9 +1,9 @@
 from os import listdir
 from os.path import isfile, join
 import path_constants
-from numpy.random import rand
+import numpy as np
 import h5py
-from beam_profile_creator import BeamProfileCreator
+from beam_profiles_pipeline import BeamProfilesPipeline
 import constants
 
 
@@ -16,14 +16,15 @@ def get_run_path(run_number=0):
     return join(directory, get_paths_to_files_in_directory(directory)[run_number])
 
 
-def get_beam_profiles(run_number=0):
+def get_beam_profiles_pipeline(run_number=0):
     if path_constants.MOCK:
-        return (rand(100, 200, 230) * constants.BEAM_PROFILE_COLOR_RESOLUTION).astype(int)
+        data = np.round(np.random.rand(100, 200, 230) * constants.BEAM_PROFILE_COLOR_RESOLUTION)
     else:
         with h5py.File(get_run_path(run_number=run_number), 'r') as current_run:
-            return current_run[path_constants.beam_profiles_path]
+            data = current_run[path_constants.beam_profiles_path]
+    return BeamProfilesPipeline(data=data)
 
 
 def get_beam_profile_creator(run_number=0, profile_number=0):
-    beam_profiles = get_beam_profiles(run_number=run_number)
-    return BeamProfileCreator(data=beam_profiles[profile_number])
+    beam_profiles = get_beam_profiles_pipeline(run_number=run_number)
+    return BeamProfilesPipeline(data=beam_profiles[profile_number])
