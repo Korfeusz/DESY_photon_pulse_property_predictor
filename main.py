2a9b19ccd846139c559414a94b1ce232bd110bec
Leo Brockhuis <brockhul@max-p3a008.desy.de>
2019-07-25 16:53:28 +0200

    implement image shifting in the pipeline

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ import time
 if __name__ == '__main__':
     image_number = 3
     t = time.time()
-    with file_tools.get_run(run_number=1) as current_run:
+    with file_tools.get_run(run_number=3) as current_run:
         beam_profiles = file_tools\
-            .get_beam_profiles_pipeline(current_run=current_run, clip_to_ten_profiles=True) \
+            .get_beam_profiles_pipeline(current_run=current_run, clip_to_ten_profiles=False) \
             .slice_horizontally(h_min=115, h_max=334)\
             .slice_vertically(v_min=100, v_max=319)\
             .remove_background(number_of_lowest_colors=4, masking_color_resolution=15)\
             .opening()\
+            .shift_to_center_of_mass()\
             .change_color_resolution(new_resolution=63)\
             .get_rounded_beam_profiles()
 
