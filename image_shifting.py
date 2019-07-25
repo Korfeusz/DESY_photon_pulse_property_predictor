ef6dd48648cd9cc9c77621a65c98ae52135cb3db
Korfeusz <brockhuis.leo@gmail.com>
2019-07-25 16:34:29 +0200

    add single image shifting to center of mass

--- image_shifting.py
+++ image_shifting.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.ndimage
 
 
 def shift_left(image, value):
@@ -17,14 +18,35 @@ def shift_down(image, value):
     return np.pad(image, ((value, 0), (0, 0)), mode='constant')[:-value, :]
 
 
-def shift_image(image, horizontal, vertical):
+def shift_by_values(image, horizontal, vertical):
     if horizontal > 0:
         image = shift_right(image, horizontal)
-    else:
+    elif horizontal < 0:
         image = shift_left(image, -horizontal)
     if vertical > 0:
         image = shift_up(image, vertical)
-    else:
+    elif vertical < 0:
         image = shift_down(image, -vertical)
     return image
 
+
+def get_geometric_center(image):
+    return np.round([x/2 for x in image.shape]).astype(np.int)
+
+
+def get_center_of_mass(image):
+     return np.array(scipy.ndimage.measurements.center_of_mass(image)).astype(np.int)
+
+
+def get_shift_values(image):
+    y_center_of_mass, x_center_of_mass = get_center_of_mass(image)
+    y_geometric, x_geometric = get_geometric_center(image)
+    return x_geometric - x_center_of_mass, -(y_geometric - y_center_of_mass)
+
+
+def shift_com_to_geometric(image):
+    horizontal, vertical = get_shift_values(image)
+    return shift_by_values(image, horizontal=horizontal, vertical=vertical)
+
+
+# e = np.random.randint(low=0, high=10, size=(10, 10))
