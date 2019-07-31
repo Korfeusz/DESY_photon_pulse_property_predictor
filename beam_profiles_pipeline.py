3cd2d4d318d755e9d6d28b16f850af797fff0228
Korfeusz <brockhuis.leo@gmail.com>
2019-07-31 16:25:11 +0200

    add new shifting method - shift to the highest intensity blob

--- beam_profiles_pipeline.py
+++ beam_profiles_pipeline.py
@@ -1,3 +1,5 @@
+import time
+
 import constants
 import numpy as np
 import background_remove_tools
@@ -12,22 +14,31 @@ class BeamProfilesPipeline:
         self.color_resolution = color_resolution
 
     def slice_horizontally(self, h_min, h_max):
-        return BeamProfilesPipeline(data=self.beam_profile_data[:, :, h_min:h_max])
+        # t = time.time()
+        data = self.beam_profile_data[:, :, h_min:h_max]
+        # print('Horizontal slice timing: {}'.format(time.time() - t))
+        return BeamProfilesPipeline(data=data)
 
     def slice_vertically(self, v_min, v_max):
-        return BeamProfilesPipeline(data=self.beam_profile_data[:, v_min:v_max, :])
+        # t = time.time()
+        data = self.beam_profile_data[:, v_min:v_max, :]
+        # print('Vertical slice timing: {}'.format(time.time() - t))
+        return BeamProfilesPipeline(data=data)
 
     def change_color_resolution(self, new_resolution):
+        # t = time.time()
         data = background_remove_tools.change_color_resolution(self.beam_profile_data,
                                                                new_resolution,
                                                                self.color_resolution)
         color_resolution = new_resolution
+        # print('Changing color resolution time: {}'.format(time.time() - t))
         return BeamProfilesPipeline(data, color_resolution)
 
     def alternative_remove_background(self, cut_off_level):
-        return BeamProfilesPipeline(background_remove_tools.alternative_remove_background(self.beam_profile_data,
-                                                                                          cut_off_level),
-                                    self.color_resolution)
+        # t = time.time()
+        data = background_remove_tools.alternative_remove_background(self.beam_profile_data, cut_off_level)
+        # print('Background remove timing: {}'.format(time.time() - t))
+        return BeamProfilesPipeline(data, self.color_resolution)
 
     def remove_background(self, number_of_lowest_colors=2, masking_color_resolution=7):
         data = background_remove_tools.remove_background(self.beam_profile_data,
@@ -37,21 +48,27 @@ class BeamProfilesPipeline:
         return BeamProfilesPipeline(data, self.color_resolution)
 
     def opening(self):
-        return BeamProfilesPipeline(background_remove_tools.grayscale_opening(self.beam_profile_data),
-                                    self.color_resolution)
+        # t = time.time()
+        data = background_remove_tools.grayscale_opening(self.beam_profile_data)
+        # print('Opening timing: {}'.format(time.time() - t))
+        return BeamProfilesPipeline(data, self.color_resolution)
 
     def shift_to_center_of_mass(self):
-        return BeamProfilesPipeline(image_shifting.shift_com_to_geometric(self.beam_profile_data),
-                                    self.color_resolution)
+        # t = time.time()
+        data = image_shifting.shift_com_to_geometric(self.beam_profile_data)
+        # print('Shifting to com timing: {}'.format(time.time() - t))
+        return BeamProfilesPipeline(data, self.color_resolution)
 
     def get_rounded_beam_profiles(self):
         return np.round(self.beam_profile_data)
 
     def rescale_images(self, horizontal_scale=1.1):
+        # t = time.time()
         data = skimage.transform.rescale(self.beam_profile_data, scale=(1, 1, horizontal_scale), multichannel=False)
         shape_corrected_data = data[:, :, :self.beam_profile_data.shape[-1]]
+        # print('Rescale timing: {}'.format(time.time() - t))
         return BeamProfilesPipeline(shape_corrected_data, color_resolution=self.color_resolution)
 
-
-    def shift_to_highest_intensity(self):
-        pass
+    def shift_to_highest_intensity(self, fraction):
+        data = image_shifting.shift_highest_intensity_to_geometric(self.beam_profile_data, fraction=fraction)
+        return BeamProfilesPipeline(data, self.color_resolution)
--- image_shifting.py
+++ image_shifting.py
@@ -1,6 +1,9 @@
 import numpy as np
 import scipy.ndimage
+import skimage.measure
+import time
 
+import binarisation_tools
 
 def shift_left(image, value):
     return np.pad(image, ((0, 0), (0, value)), mode='constant')[:, value:]
@@ -44,6 +47,12 @@ def get_center_of_mass(image):
     return np.array(scipy.ndimage.measurements.center_of_mass(image)).astype(np.int)
 
 
+def get_shift_values(geometric_center, new_center):
+    y_geo, x_geo = geometric_center
+    y_new, x_new = new_center
+    return x_geo - x_new, -(y_geo - y_new)
+
+
 def get_shift_to_com_values(image):
     y_center_of_mass, x_center_of_mass = get_center_of_mass(image)
     y_geometric, x_geometric = get_geometric_center(image)
@@ -59,5 +68,54 @@ def shift_com_to_geometric(data):
     return np.array(list(map(shift_com_to_geometric_single_image, data)))
 
 
-def shift_highest_intensity_to_geometric(data):
-    pass
+def shift_highest_intensity_to_geometric(data, fraction):
+    binarised_data = binarisation_tools.binarise_by_fraction(data, fraction)
+    new_data = np.zeros(shape=data.shape)
+    t = time.time()
+    for i, image in enumerate(binarised_data):
+        label_image = skimage.measure.label(image)
+        regions = skimage.measure.regionprops(label_image)
+        if regions:
+            areas = [prop.area for prop in regions]
+            centers = [[x.astype(np.int) for x in prop.centroid] for prop in regions]
+            max_index = np.argmax(areas)
+            peak_center = centers[max_index]
+            geom_center = get_geometric_center(data[0, :, :])
+            horizontal, vertical = get_shift_values(geom_center, peak_center)
+            shifted_image = shift_by_values(data[i, :, :], horizontal, vertical)
+        else:
+            shifted_image = data[i, :, :]
+        new_data[i, :, :] = shifted_image
+    print(time.time() - t)
+    return new_data
+
+
+if __name__ == '__main__':
+    from gaussian_fit_tools import two_dim_symmetric_gaussian_function, two_dim_asymmetric_gaussian_function
+    import matplotlib.pyplot as plt
+
+    def create_grid(image):
+        vertical_size, horizontal_size = image.shape
+        grid_bound_vertical = vertical_size / 2 - 0.5
+        grid_bound_horizontal = horizontal_size / 2 - 0.5
+        x = np.linspace(-grid_bound_horizontal, grid_bound_horizontal, horizontal_size)
+        y = np.linspace(-grid_bound_vertical, grid_bound_vertical, vertical_size)
+        return np.meshgrid(x, y)
+    size = (219, 219)
+    grid = create_grid(np.zeros(size))
+    gauss_1 = two_dim_asymmetric_gaussian_function(grid, 10, 5, 7, 15, 23).reshape(size)
+    gauss_2 = two_dim_asymmetric_gaussian_function(grid, 10, -20, -50, 20, 15).reshape(size)
+    gauss = gauss_1 + gauss_2
+    # gauss = gauss +  np.random.random(219*219).reshape((219, 219))
+    gauss = gauss.round()
+    plt.imshow(gauss)
+    plt.show()
+    data = np.array([gauss, gauss])
+
+
+
+    dat =shift_highest_intensity_to_geometric(data, fraction=0.9)
+
+    plt.imshow(dat[0, :, :])
+    plt.show()
+
