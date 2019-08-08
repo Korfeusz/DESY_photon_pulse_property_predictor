95ed8fdc28bfda1e98d925be52c25ddcfcc627ae
Leo Brockhuis <brockhul@max-p3a006.desy.de>
2019-08-08 17:54:00 +0200

    refactor labeling_algo started

--- beam_profile_metadata/labeling_algo.py
+++ beam_profile_metadata/labeling_algo.py
@@ -1,23 +1,8 @@
 import numpy as np
+from beam_profile_metadata.labeling_tools import get_specific_circle_indices_list
 from imaging_tools import beam_profile_imaging
 from beam_profile_metadata import beam_profile_metadata_tools, tools
 from json_tools import json_tools
-
-
-def get_specific_circle_indices_list(beam_profiles_metadata_dict, experiment_name,
-                                     index_type,
-                                     binarisation_fraction=None, ring_thickness=None,
-                                     number_of_tests=None):
-    beam_profiles_found = []
-    circle_indices = []
-    index_string = beam_profile_metadata_tools.get_circle_index_string(experiment_name, index_type, binarisation_fraction, ring_thickness,
-                                                                       number_of_tests)
-
-    for profile_index, profile_metadata in beam_profiles_metadata_dict.items():
-        if not profile_metadata['corrupted'] and index_string in profile_metadata['circularity_index'].keys():
-            beam_profiles_found.append(profile_index)
-            circle_indices.append(profile_metadata['circularity_index'][index_string]['value'])
-    return beam_profiles_found, circle_indices
 
 
 def get_split_above_and_below_threshold(beam_profiles, circle_indices, threshold):
