e1e47f1144cf5c6ce4f6bfab0192f8c780c1c018
Korfeusz <brockhuis.leo@gmail.com>
2019-08-09 16:15:56 +0200

    add labeling to metadata writer

--- beam_profile_metadata/labeling_tools.py
+++ beam_profile_metadata/labeling_tools.py
@@ -1,17 +1,29 @@
-from beam_profile_metadata import beam_profile_metadata_tools
+from beam_profile_metadata.circularity_index_entries import CircularityIndexEntries
+from beam_profile_metadata import dictionary_tools
 
 
-def get_specific_circle_indices_list(beam_profiles_metadata_dict, experiment_name,
-                                     index_type,
-                                     binarisation_fraction=None, ring_thickness=None,
-                                     number_of_tests=None):
-    beam_profiles_found = []
-    circle_indices = []
-    index_string = beam_profile_metadata_tools.get_circle_index_string(experiment_name, index_type, binarisation_fraction, ring_thickness,
-                                                                       number_of_tests)
+def label_by_threshold(metadata_dict, circularity_entries: CircularityIndexEntries, threshold):
+    index_string = circularity_entries.index_string
+    for profile, index in circularity_entries.get_profile_names_and_their_circle_indices(metadata_dict):
+        dictionary_tools.insert_value_at_end_of_keys(metadata_dict, keys=[profile, 'label', index_string, 'value'],
+                                                     value=int(index < threshold))
+        dictionary_tools.insert_keyval_without_overwriting(metadata_dict[profile]['label'][index_string],
+                                                           key='threshold', value=threshold)
 
-    for profile_index, profile_metadata in beam_profiles_metadata_dict.items():
-        if not profile_metadata['corrupted'] and index_string in profile_metadata['circularity_index'].keys():
-            beam_profiles_found.append(profile_index)
-            circle_indices.append(profile_metadata['circularity_index'][index_string]['value'])
-    return beam_profiles_found, circle_indices
+
+def get_profiles_with_existing_specific_labels(metadata_dict, index_string):
+    return [p for p in metadata_dict if 'label' in metadata_dict[p] and index_string in metadata_dict[p]['label']]
+
+
+def label_by_combination(metadata_dict, circularity_entries_1: CircularityIndexEntries,
+                         circularity_entries_2: CircularityIndexEntries, label_name):
+    index_string_1 = circularity_entries_1.index_string
+    index_string_2 = circularity_entries_2.index_string
+    profiles_1 = get_profiles_with_existing_specific_labels(metadata_dict, index_string_1)
+    profiles_2 = get_profiles_with_existing_specific_labels(metadata_dict, index_string_2)
+    profiles_intersect = [p for p in profiles_1 if p in profiles_2]
+    for profile in profiles_intersect:
+        value = metadata_dict[profile]['label'][index_string_1]['value'] \
+                or metadata_dict[profile]['label'][index_string_2]['value']
+        dictionary_tools.insert_value_at_end_of_keys(metadata_dict[profile]['label'], keys=[label_name, 'value'],
+                                                     value=value)
--- profile_import_and_preprocessing.py
+++ profile_import_and_preprocessing.py
@@ -12,5 +12,5 @@ if __name__ == '__main__':
                                                             run_input=run_input, indent=2)
         beam_profiles = beam_profiles_preprocessing.get_beam_profiles_from_dict(run_input)
         beam_profiles_raw = beam_profiles_preprocessing.get_raw_beam_profiles_from_dict(run_input)
-        np.save('preprocessed_data/beam_profiles_run_{}_raw'.format(run_number), beam_profiles_raw)
-        np.save('preprocessed_data/beam_profiles_run_{}'.format(run_number), beam_profiles)
+        np.save('preprocessed_data/beam_profiles_run_{}_raw_small'.format(run_number), beam_profiles_raw)
+        np.save('preprocessed_data/beam_profiles_run_{}_small'.format(run_number), beam_profiles)
