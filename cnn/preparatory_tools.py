f0cb050a39c6baf6d29400e5b9d8cbbe9e9a0596
Leo Brockhuis <brockhul@max-p3a007.desy.de>
2019-08-12 16:33:57 +0200

    clean cnn folder

--- cnn/preparatory_tools.py
+++ cnn/preparatory_tools.py
@@ -6,17 +6,3 @@ def one_hot(data, num_classes):
     one_hot[np.arange(length), data] = 1
     return one_hot
 
-
-def next_batch(remaining_set, training_set, batch_size=64):
-    x, y = remaining_set
-    length = x.shape[0]
-    if batch_size > length:
-        return (x, y), training_set
-    mask = np.zeros(length, dtype=np.bool)
-    idx = np.random.choice(length, batch_size, replace=False)
-    mask[idx] = True
-    x_batch = x[mask]
-    x_rest = x[~mask]
-    y_batch = y[mask]
-    y_rest = y[~mask]
-    return (x_batch, y_batch), (x_rest, y_rest)
