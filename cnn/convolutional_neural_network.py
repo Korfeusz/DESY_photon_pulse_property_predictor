ea2367a1089ce07d5bd5c1c871fbf37309019b4c
Leo Brockhuis <brockhul@max-p3ag002.desy.de>
2019-08-12 17:51:58 +0200

    finish basic cnn

--- cnn/convolutional_neural_network.py
+++ cnn/convolutional_neural_network.py
@@ -4,6 +4,7 @@ from cnn import preparatory_tools
 import numpy as np
 import tensorflow as tf
 import datetime
+import cnn
 
 if __name__ == '__main__':
     metadata_dict = json_tools.import_json_as_dict('../metadata/meta_test_1.json')
@@ -11,63 +12,26 @@ if __name__ == '__main__':
                                                                                      label_name='experimental_combo',
                                                                                      path_to_data='../preprocessed_data/beam_profiles_run_{}_raw_small.npy',
                                                                                      runs=[0, 1, 2, 3])
-    shape_0, shape_1 = x_train.shape[-2:]
+
     y_train_1h = preparatory_tools.one_hot(y_train, 2)
     y_test_1h = preparatory_tools.one_hot(y_test, 2)
-    x_train_reshaped = np.reshape(x_train, (-1, shape_0, shape_1, 1))
-    x_test_reshaped = np.reshape(x_test, (-1, shape_0, shape_1, 1))
 
-    model = tf.keras.Sequential(
-        [
-            tf.keras.layers.Reshape(input_shape=(shape_0, shape_1,), target_shape=(shape_0, shape_1, 1)),
 
-            tf.keras.layers.Conv2D(kernel_size=3, filters=12, use_bias=False, padding='same'),
-            tf.keras.layers.BatchNormalization(center=True, scale=False),
-            tf.keras.layers.Activation('relu'),
-
-            tf.keras.layers.Conv2D(kernel_size=6, filters=24, use_bias=False, padding='same', strides=2),
-            tf.keras.layers.BatchNormalization(center=True, scale=False),
-            tf.keras.layers.Activation('relu'),
-
-            tf.keras.layers.Conv2D(kernel_size=6, filters=32, use_bias=False, padding='same', strides=2),
-            tf.keras.layers.BatchNormalization(center=True, scale=False),
-            tf.keras.layers.Activation('relu'),
-
-            tf.keras.layers.Flatten(),
-
-            tf.keras.layers.Dense(200, use_bias=False),
-            tf.keras.layers.BatchNormalization(center=True, scale=False),
-            tf.keras.layers.Activation('relu'),
-
-            tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(2, activation='softmax')
-        ])
-
+    model = cnn.get_model(x_train.shape[-2:])
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.01),
                   loss='categorical_crossentropy',
                   metrics=['accuracy'])
 
     # print model layers
-    model.summary()
+    # model.summary()
+    lr_decay_callback = tf.keras.callbacks.LearningRateScheduler(cnn.cnn_tools.lr_decay, verbose=True)
 
 
-    # lr decay function
-    def lr_decay(epoch):
-        return 0.01 * np.power(0.666, epoch)
-
-
-    # lr schedule callback
-    lr_decay_callback = tf.keras.callbacks.LearningRateScheduler(lr_decay, verbose=True)
-
-
-    BATCH_SIZE = 50
+    BATCH_SIZE = 20
     steps_per_epoch = 100 // BATCH_SIZE  # 60,000 items in this dataset
-    print("Steps per epoch: ", steps_per_epoch)
-
     log_dir = "../logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
     tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-
-    history = model.fit(x_train, y_train_1h, steps_per_epoch=steps_per_epoch, epochs=2,
+    history = model.fit(x_train, y_train_1h, batch_size=BATCH_SIZE, steps_per_epoch=steps_per_epoch, epochs=100,
                         validation_data=(x_test, y_test_1h), validation_steps=1,
                         callbacks=[lr_decay_callback, tensorboard_callback])
 
---
+++ cnn/network_model.py
@@ -0,0 +1,30 @@
+import tensorflow as tf
+
+
+def get_model(image_shape):
+    shape_0, shape_1 = image_shape
+    return tf.keras.Sequential(
+            [
+                tf.keras.layers.Reshape(input_shape=(shape_0, shape_1,), target_shape=(shape_0, shape_1, 1)),
+
+                tf.keras.layers.Conv2D(kernel_size=3, filters=12, use_bias=False, padding='same'),
+                tf.keras.layers.BatchNormalization(center=True, scale=False),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Conv2D(kernel_size=6, filters=24, use_bias=False, padding='same', strides=2),
+                tf.keras.layers.BatchNormalization(center=True, scale=False),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Conv2D(kernel_size=6, filters=32, use_bias=False, padding='same', strides=2),
+                tf.keras.layers.BatchNormalization(center=True, scale=False),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Flatten(),
+
+                tf.keras.layers.Dense(200, use_bias=False),
+                tf.keras.layers.BatchNormalization(center=True, scale=False),
+                tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Dropout(0.3),
+                tf.keras.layers.Dense(2, activation='softmax')
+            ])
