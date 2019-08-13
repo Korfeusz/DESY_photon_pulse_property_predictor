cb30f0c7454e30249d14cdaa434e1e34b34aad9f
Korfeusz <brockhuis.leo@gmail.com>
2019-08-13 09:24:21 +0200

    reformat labeling insights

--- beam_profile_metadata/labeling_insights.py
+++ beam_profile_metadata/labeling_insights.py
@@ -3,17 +3,32 @@ import numpy as np
 
 
 def get_profiles_labeled_1_with_worst_index(metadata_dict, circularity_entries, number_to_find):
-    profile_names, circularity_indices = list(zip(
-        *list(circularity_entries.get_profile_names_and_their_circle_indices(metadata_dict))))
-    index_string = circularity_entries.index_string
-    indices_of_beam_profiles_labeled_1 = [i for i, p in enumerate(profile_names) if
-                                          metadata_dict[p]['label'][index_string]['value'] == 1]
-
-    if len(indices_of_beam_profiles_labeled_1) < number_to_find:
-        number_to_find = len(indices_of_beam_profiles_labeled_1)
-
-    beam_profiles_labeled_1 = np.array(profile_names)[indices_of_beam_profiles_labeled_1]
-    circle_indices_of_beam_profiles_labeled_1 = np.array(circularity_indices)[indices_of_beam_profiles_labeled_1]
-    worst_indices = tools.get_indices_of_n_highest_values(np.array(circle_indices_of_beam_profiles_labeled_1),
-                                                          number_to_find)
+    profile_names, circularity_indices = get_profile_names_and_circularity_indices(circularity_entries, metadata_dict)
+    indices_of_beam_profiles_labeled_1 = get_indices_of_beam_profiles_labeled_1(profile_names, metadata_dict,
+                                                                                circularity_entries.index_string)
+    beam_profiles_labeled_1 = fancy_index_list_and_convert_to_array(profile_names, indices_of_beam_profiles_labeled_1)
+    circle_indices_of_beam_profiles_labeled_1 = fancy_index_list_and_convert_to_array(circularity_indices,
+                                                                                      indices_of_beam_profiles_labeled_1)
+    worst_indices = get_worst_indices(circle_indices_of_beam_profiles_labeled_1, number_to_find)
     return beam_profiles_labeled_1[worst_indices], circle_indices_of_beam_profiles_labeled_1[worst_indices]
+
+
+def get_profile_names_and_circularity_indices(circularity_entries, metadata_dict):
+    return list(zip(
+        *list(circularity_entries.get_profile_names_and_their_circle_indices(metadata_dict))))
+
+
+def get_indices_of_beam_profiles_labeled_1(profile_names, metadata_dict, index_string):
+    return [i for i, p in enumerate(profile_names) if
+            metadata_dict[p]['label'][index_string]['value'] == 1]
+
+
+def fancy_index_list_and_convert_to_array(input_list, indices):
+    return np.array(input_list)[indices]
+
+
+def get_worst_indices(circle_indices, number_to_find):
+    if len(circle_indices) < number_to_find:
+        number_to_find = len(circle_indices)
+    return tools.get_indices_of_n_highest_values(circle_indices,
+                                                 number_to_find)
--- profiles_visualisation.py
+++ profiles_visualisation.py
@@ -2,22 +2,26 @@ import beam_profile_metadata
 import imaging_tools
 import json_tools
 
+
 if __name__ == '__main__':
     metadata_file = 'metadata/metadata.json'
+    experiment_name = '0'
+    run_inputs_file = 'metadata/run_inputs.json'
+
     metadata_dict = json_tools.import_json_as_dict(metadata_file)
 
     masking_entries = beam_profile_metadata.MaskingIndexEntries(ring_thickness=10, number_of_tests=2,
-                                                                experiment_name='0')
+                                                                experiment_name=experiment_name)
     area_perimeter_entries = beam_profile_metadata.AreaPerimeterIndexEntries(binarisation_fractions=[0.3, 0.5, 0.7],
-                                                                             experiment_name='0')
+                                                                             experiment_name=experiment_name)
     worst_profiles, worst_indices = beam_profile_metadata.get_profiles_labeled_1_with_worst_index(metadata_dict,
                                                                                                   masking_entries,
                                                                                                   number_to_find=100)
 
-    print(worst_indices)
-    print(worst_profiles)
+    print('worst indices: {}'.format(worst_indices))
+    print('worst profiles: {}'.format(worst_profiles))
     images = imaging_tools.get_profiles_from_indices(worst_profiles,
                                                      metadata_file=metadata_file,
-                                                     run_inputs_file='metadata/run_inputs.json',
-                                                     experiment_name='0')
+                                                     run_inputs_file=run_inputs_file,
+                                                     experiment_name=experiment_name)
     imaging_tools.show_images(images, rows=10)
