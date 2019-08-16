f8b20fb30b3645eaa35de9f6ad85944aac74aa19
Korfeusz <brockhuis.leo@gmail.com>
2019-08-16 12:51:14 +0200

    add saved network models to gitignore

--- cnn/cnn_tools.py
+++ cnn/cnn_tools.py
@@ -2,4 +2,6 @@ import numpy as np
 
 
 def lr_decay(epoch):
-    return 0.01 * np.power(0.666, epoch)
+    # return 0.01 * np.power(0.666, epoch)
+    return 0.01 * np.power(0.95, epoch)
+
--- cnn/network_model.py
+++ cnn/network_model.py
@@ -7,17 +7,23 @@ def get_model(image_shape):
             [
                 tf.keras.layers.Reshape(input_shape=(shape_0, shape_1,), target_shape=(shape_0, shape_1, 1)),
 
-                tf.keras.layers.Conv2D(kernel_size=3, filters=6, use_bias=False, padding='same'),
+                tf.keras.layers.Conv2D(kernel_size=3, filters=12, use_bias=False, padding='same'),
                 tf.keras.layers.BatchNormalization(center=True, scale=False),
                 tf.keras.layers.Activation('relu'),
 
-                tf.keras.layers.Conv2D(kernel_size=6, filters=6, use_bias=False, padding='same', strides=2),
+                tf.keras.layers.Dropout(0.5),
+
+                tf.keras.layers.Conv2D(kernel_size=6, filters=24, use_bias=False, padding='same', strides=2),
                 tf.keras.layers.BatchNormalization(center=True, scale=False),
                 tf.keras.layers.Activation('relu'),
+
+                tf.keras.layers.Dropout(0.5),
 
                 # tf.keras.layers.Conv2D(kernel_size=6, filters=32, use_bias=False, padding='same', strides=2),
                 # tf.keras.layers.BatchNormalization(center=True, scale=False),
                 # tf.keras.layers.Activation('relu'),
+                #
+                # tf.keras.layers.Dropout(0.5),
 
                 tf.keras.layers.Flatten(),
 
@@ -25,6 +31,6 @@ def get_model(image_shape):
                 tf.keras.layers.BatchNormalization(center=True, scale=False),
                 tf.keras.layers.Activation('relu'),
 
-                tf.keras.layers.Dropout(0.3),
+                tf.keras.layers.Dropout(0.5),
                 tf.keras.layers.Dense(2, activation='softmax')
             ])
