425a6aa96beae6bcaa4c05250f6e8078e2a7bf25
Korfeusz <brockhuis.leo@gmail.com>
2019-08-19 10:59:10 +0200

    add plot of ratio of 0 to 1

--- model_evaluation.py
+++ model_evaluation.py
@@ -4,7 +4,8 @@ import json_tools
 
 if __name__ == '__main__':
     metadata_dict = json_tools.import_json_as_dict('metadata/metadata.json')
-    all_data = cnn_evaluation.get_all_uncorrupted_beam_profiles(data_storage_filename='/beegfs/desy/user/brockhul/preprocessed_data/beam_profiles_run_{}_raw_downsized.npy',
+    data_storage_filename='/beegfs/desy/user/brockhul/preprocessed_data/beam_profiles_run_{}_raw_downsized.npy'
+    all_data = cnn_evaluation.get_all_uncorrupted_beam_profiles(data_storage_filename=data_storage_filename,
                                                    metadata_dict=metadata_dict)
 
     print(all_data.shape)
@@ -12,3 +13,6 @@ if __name__ == '__main__':
     predictions = cnn_evaluation.get_predictions_from_model('model/model_5.h5', all_data)
     cnn_evaluation.show_random_100_images_with_labels(images=all_data, predicted_label=predictions,
                                                       correct_label=None)
+    cnn_evaluation.plot_0_1_composition_of_runs(data_storage_filename=data_storage_filename,
+                                                metadata_dict=metadata_dict,
+                                                model_file='model/model_5.h5')
